032b0562baa81e91f20ee36fdcffb2766fcfc861
Krishan-Kumar-Prajapati <krishan.kumar@alumni.iitgn.ac.in>
2023-01-22 18:47:54 +0530

    fw23_0121_Day_5

--- cart.js
+++ cart.js
@@ -1,16 +1,25 @@
 let myCartData=JSON.parse(localStorage.getItem("myCartData")) || [];
 
     let maincontainer=document.getElementById("K131");
+    let MyShoppingBag=document.getElementById("MyShoppingBag");
     let Subtotal=document.getElementById("Subtotal");
     let Taxes=document.getElementById("Taxes");
-    let cupon=document.querySelector("#cupon > input");
-    let cuponbtn=document.querySelector("#cupon > button");
+    let Estimatedtotal=document.getElementById("Estimatedtotal");
+    let cupon=document.getElementById("cupon");
+    let cuponbtn=document.getElementById("cuponbtn");
     let Numberoftimes=0;
 
 
 
 
     let totalpay;
+    if(myCartData.length==0){
+      MyShoppingBag.innerHTML=null;
+      MyShoppingBag.innerHTML=`Your shopping bag is empty`;
+    }else{
+      MyShoppingBag.innerHTML=null;
+      MyShoppingBag.innerHTML=`My Shopping Bag`;
+    }
     
 
     createdom(myCartData);
@@ -20,7 +29,7 @@ let myCartData=JSON.parse(localStorage.getItem("myCartData")) || [];
 
     function createdom(data){
         maincontainer.innerHTML=null;
-      let sum=0;
+        let sum=0;
 
 
 
@@ -134,8 +143,15 @@ let myCartData=JSON.parse(localStorage.getItem("myCartData")) || [];
 
 
       })
-      totalpay
+      Subtotal.innerHTML=null;
+        Taxes.innerHTML=null;
+        Estimatedtotal.innerHTML=null;
+      totalpay=(sum+Math.floor(0.18*sum));
+
       Subtotal.innerHTML=`$ ${sum}`;
+      Taxes.innerHTML=`$ ${Math.floor(0.18*sum)}`;
+      Estimatedtotal.innerHTML=`$ ${totalpay}`;
+
       
 
 
@@ -148,96 +164,145 @@ let myCartData=JSON.parse(localStorage.getItem("myCartData")) || [];
 
 
     cuponbtn.addEventListener("click",function(){
-      if(cupon.value == "Masai30" && Numberoftimes<1){
-        totalpay=Math.floor(0.7*totalpay)
+      if(cupon.value == "CLARKS20" && Numberoftimes<1){
+        alert("Promo Code Applied Successfully")
+        totalpay=Math.floor(0.8*totalpay)
         Numberoftimes++;
+        // let sum=0;
 
-        
-      LSdata.forEach(function(element,index){
-        
-
-        let div=document.createElement("div")
-        let img=document.createElement("img")
-        img.setAttribute("src",element.image)
-        let title=document.createElement("h2")
-        title.innerHTML=element.title;
-        let ingredients=document.createElement("p")
-        ingredients.innerHTML=element.ingredients.join(" ");
-        let description=document.createElement("p")
-        description.innerHTML=element.description;
-        let price=document.createElement("h3");
-        price.innerHTML=`₹ ${element.price}`;
-
-        let plusbtn=document.createElement("button");
-        plusbtn.innerHTML='+';
-        plusbtn.addEventListener("click",function(){
-          element.qu=element.qu+1;
-          createdom(LSdata);
-
-
-        })
-
-        let show=document.createElement("p")
-        show.innerHTML=element.qu;
-
-
-
-        let minusbtn=document.createElement("button");
-        minusbtn.innerHTML='-';
-
-        minusbtn.addEventListener("click",function(){
-          
-          element.qu=element.qu-1;
-          createdom(LSdata);
-          
-
-        })
-
-
-
-
-
-
-        let Cancelbtn=document.createElement("button");
-        Cancelbtn.innerHTML='Cancel';
-        Cancelbtn.addEventListener("click",function(){
-          let aftercancel=LSdata.filter(function(e,i){
-            if(element==e){
-              return false;
-            }else{
-              return true;
-            }
-          })
-          LSdata=aftercancel;
-          localStorage.setItem("buy",JSON.stringify(LSdata));
-          createdom(LSdata);
-
-
-
-        })
-       
-
-        div.append(img,title,ingredients,description,price,plusbtn,show,minusbtn,Cancelbtn);
-        menucontainer.append(div);
-
-
-
-
-
-
-
-
-
-
-
-      })
       
-      ordertotal.innerHTML=totalpay;
+        // data.forEach(function(element,index){
+        //   sum=sum+((element.quantity)*(element.price))
+  
+        //   let firstdiv=document.createElement("div");
+  
+        //   let img=document.createElement("img")
+        //   img.setAttribute("src",element.image)
+  
+        //   firstdiv.append(img);
+  
+        //   let seconddiv=document.createElement("div");
+  
+  
+        //   let secondfirstdiv=document.createElement("div"); 
+            
+        //   let mytype=document.createElement("h3");
+        //   mytype.innerText=element.type;
+        //   let myprice=document.createElement("h3");
+        //   myprice.innerText=`$ ${element.price}`;
+  
+        //   secondfirstdiv.append(mytype,myprice)
+        //   seconddiv.append(secondfirstdiv)
+  
+        //   let mymaterial=document.createElement("p");
+        //   mymaterial.innerText=element.material;
+  
+        //   let description=document.createElement("p")
+        //   description.innerHTML="Size 7, Medium";
+  
+        //   seconddiv.append(mymaterial,description);
+  
+        //   let secondseconddiv=document.createElement("div");
+  
+          
+  
+        //   let plusbtn=document.createElement("button");
+        //   plusbtn.innerHTML='+';
+        //   plusbtn.addEventListener("click",function(){
+        //     element.quantity=element.quantity+1;
+        //     createdom(myCartData);
+  
+  
+        //   })
+  
+        //   let showdiv=document.createElement("div");
+  
+        //   let show=document.createElement("p")
+        //   show.innerHTML=element.quantity;
+  
+        //   showdiv.append(show)
+  
+  
+  
+  
+  
+        //   let minusbtn=document.createElement("button");
+        //   minusbtn.innerHTML='-';
+  
+        //   minusbtn.addEventListener("click",function(){
+            
+        //     element.quantity=element.quantity-1;
+        //     createdom(myCartData);
+            
+  
+        //   })
+  
+        //   secondseconddiv.append(plusbtn,showdiv,minusbtn)
+        //   seconddiv.append(secondseconddiv)
+  
+  
+  
+  
+  
+  
+  
+        //   let Cancelbtn=document.createElement("button");
+        //   Cancelbtn.innerHTML='Remove';
+        //   Cancelbtn.addEventListener("click",function(){
+        //     let aftercancel=myCartData.filter(function(e,i){
+        //       if(element==e){
+        //         return false;
+        //       }else{
+        //         return true;
+        //       }
+        //     })
+        //     myCartData=aftercancel;
+        //     localStorage.setItem("myCartData",JSON.stringify(myCartData));
+        //     createdom(myCartData);
+  
+  
+  
+        //   })
+  
+        //   seconddiv.append(Cancelbtn)
+        //   maincontainer.append(firstdiv,seconddiv);
+         
+  
+          
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+        // })
+        
+      //   Subtotal.innerHTML=null;
+      //   Taxes.innerHTML=null;
+        Estimatedtotal.innerHTML=null;
+      // totalpay=(sum+Math.floor(0.18*sum))*0.8;
+
+      // Subtotal.innerHTML=`$ ${sum}`;
+      // Taxes.innerHTML=`$ ${Math.floor(0.18*sum)}`;
+      Estimatedtotal.innerHTML=`$ ${totalpay}`;
 
 
 
 
 
+      }else if (cupon.value !== "CLARKS20"){
+        alert("Apply Correct Promo Code")
+
+      }else if (Numberoftimes>=1){
+        alert("Promo Code Already Redeemed")
+
+      }else{
+        alert("Apply Correct Promo Code")
       }
 
 
--- oneproduct.js
+++ oneproduct.js
@@ -1,12 +1,18 @@
-let mymid=document.getElementById("mid");
-let Shoppingbagbtn=document.getElementById("Shoppingbagbtn")
+/* <!-- Neha's work date 21/1/23 to be added in main--> */
 
-let myfirstdiv=document.getElementById("firstdiv");
-let seconddivdiv=document.getElementById("seconddiv");
-let mythirddiv=document.getElementById("thirddiv");
-let myforthdiv=document.getElementById("forthdiv");
-let myfifthdiv=document.getElementById("fifthdiv");
-let myRoundbox=document.getElementById("Roundbox");
+let mymid=document.getElementById("mid");
+
+
+    let first = document.getElementById("first");
+    let second = document.getElementById("second");
+    let third = document.getElementById("third");
+    let forth = document.getElementById("forth");
+    let fifth = document.getElementById("fifth");
+
+let roundimg = document.getElementById("roundimg");
+
+
+let Shoppingbagbtn = document.getElementById("Shoppingbagbtn")
 
 
 let LSdata=JSON.parse(localStorage.getItem("current"));
@@ -15,30 +21,85 @@ if(LSdata==null){
 }
 LSdata.quantity=1;
 
+// //ADD YOUR CODE HERE:
+// Shoppingbagbtn.addEventListener("click",()=>{
+//   //ADD YOUR CODE HERE
+//   alert("ADD YOUR CODE HERE")
+  
+// })
+
+// let LSdata=JSON.parse(localStorage.getItem("current"));
+// if(LSdata==null){
+//   LSdata={};
+// }
+
 let mymidimg=document.createElement("img");
 mymidimg.setAttribute("src",LSdata.image);
 
 mymid.innerHTML=null;
 mymid.append(mymidimg);
 
-myfirstdiv.innerHTML=null;
-myfirstdiv.append(mymidimg);
+// 
+
+// myRoundbox.innerHTML=null;
+// myRoundbox.append(mymidimg);
+roundimg.innerHTML=null;
+roundimg.setAttribute("src",LSdata.image)
+
+first.innerHTML=null;
+first.setAttribute("src",LSdata.image)
+
+second.innerHTML=null;
+second.setAttribute("src",LSdata.image)
+
+third.innerHTML=null;
+third.setAttribute("src",LSdata.image)
+
+forth.innerHTML=null;
+forth.setAttribute("src",LSdata.image)
+
+fifth.innerHTML=null;
+fifth.setAttribute("src",LSdata.image)
+
+ /* <!-- Neha's work date 21/1/23 to be added in main--> */
 
 
-seconddivdiv.innerHTML=null;
-seconddivdiv.append(mymidimg);
+// let mymid=document.getElementById("mid");
+// let Shoppingbagbtn=document.getElementById("Shoppingbagbtn")
 
-mythirddiv.innerHTML=null;
-mythirddiv.append(mymidimg);
+// let myfirstdiv=document.getElementById("firstdiv");
+// let seconddivdiv=document.getElementById("seconddiv");
+// let mythirddiv=document.getElementById("thirddiv");
+// let myforthdiv=document.getElementById("forthdiv");
+// let myfifthdiv=document.getElementById("fifthdiv");
+// let myRoundbox=document.getElementById("Roundbox");
 
-myforthdiv.innerHTML=null;
-myforthdiv.append(mymidimg);
 
-myfifthdiv.innerHTML=null;
-myfifthdiv.append(mymidimg);
 
-myRoundbox.innerHTML=null;
-myRoundbox.append(mymidimg);
+// let mymidimg=document.createElement("img");
+// mymidimg.setAttribute("src",LSdata.image);
+
+// mymid.innerHTML=null;
+// mymid.append(mymidimg);
+
+// myfirstdiv.innerHTML=null;
+// myfirstdiv.append(mymidimg);
+
+
+// seconddivdiv.innerHTML=null;
+// seconddivdiv.append(mymidimg);
+
+// mythirddiv.innerHTML=null;
+// mythirddiv.append(mymidimg);
+
+// myforthdiv.innerHTML=null;
+// myforthdiv.append(mymidimg);
+
+// myfifthdiv.innerHTML=null;
+// myfifthdiv.append(mymidimg);
+
+// myRoundbox.innerHTML=null;
+// myRoundbox.append(mymidimg);
 
 let myCartData=JSON.parse(localStorage.getItem("myCartData")) || [];
 
