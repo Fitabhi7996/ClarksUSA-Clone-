35791509ab29d8b1c22182d87b0d10d36b9169db
NehaP0 <115461376+NehaP0@users.noreply.github.com>
2023-01-22 17:45:29 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # abject-history-7359
+# ClarksUSA (Clone)
+-----
+### For Educational And Learning Purpose*
+Project: make a clone of ClarksUSA website.
+
+Original-Website: https://www.clarksusa.com/
+
+Cloned-Website: https://github.com/Fitabhi7996/abject-history-7359
+
+This is an E-Commerce website which speciaizes in selling Shoes for all age groups. The website provides 33,000 products of different types Shoes.
+
+In this project we have tried to manipulate dom elements, styling using css and tried to make website more dynamic and responsive. With our efforts and the technology stack, that we have learned till Unit-3 in the Masai School, we were able to clone the whole website with same looking & features.
+
+# Technology Stack Used 🌟
+* HTML
+* CSS
+* JavaScript
+
+For storing user data we also used localStorage.
+
+# Snapshots 📷
+* Landing page (Home page)
+
+![Landing page (Home page)]()
+
+* Product page
+
+![Product page]()
+
+
+* Checkout page
+
+![Checkout page]()
+
+* Login page
+
+![Login page]()
+
+* Success page
+
+![Success page]()
+
+## How to run the Project
+* Open pages directory.
+* Open index.html using live server.
+
+## End Notes 📑
+In this project we were tried to achieve a near to perfect clone of the original website as much as possible. This entire journey of this construct week has given us experiences and we have learned lots of things by applying to the real website and it gave us a lot of confidence. Most of the features are added and functionality of the website is achieved.
+
+## Limitations
+All pages are not responsive yet , those pages are might not be properly visible on small screen devices.
---
+++ bestsellers.js
@@ -0,0 +1,202 @@
+let mycontainer=document.getElementById("K722");
+let myfilterKS1=document.getElementById("KS1");
+let myfilterKS711=document.getElementById("KS711");
+let myfilterKS712=document.getElementById("KS712");
+let Forfilter=[];
+ 
+    let myheading=document.getElementById("count");
+    let count=0;
+ 
+    let LSdata=JSON.parse(localStorage.getItem("current"));
+    if(LSdata==null){
+      LSdata=[];
+    }
+
+
+    myfilterKS1.addEventListener("change",function(){
+        if(myfilterKS1.value=="ascending"){
+            let afterfilter= Forfilter.sort((a, b) => {
+                const nameA = a.type.toUpperCase();
+                const nameB = b.type.toUpperCase(); 
+                if (nameA < nameB) {
+                  return -1;
+                }
+                if (nameA > nameB) {
+                  return 1;
+                }
+              
+                
+                return 0;
+              });
+              createDOM(afterfilter);
+
+
+        }
+        if(myfilterKS1.value=="descending"){
+            let afterfilter=Forfilter.sort((a, b) => {
+                const nameA = a.type.toUpperCase();
+                const nameB = b.type.toUpperCase(); 
+                if (nameA < nameB) {
+                  return 1;
+                }
+                if (nameA > nameB) {
+                  return -1;
+                }
+              
+                
+                return 0;
+              });
+              createDOM(afterfilter);
+
+
+        }
+
+
+
+
+
+    })
+
+
+    myfilterKS712.addEventListener("change",function(){
+      
+      if(myfilterKS712.value=="0-100"){
+        let myfiltered=Forfilter.filter(function(element,index){
+          if(element.price >=0 && element.price<100){
+            return true;
+          }else{
+            return false;
+          }
+          
+        })
+        createDOM(myfiltered);
+        
+      }
+      if(myfilterKS712.value=="100-200"){
+        let myfiltered=Forfilter.filter(function(element,index){
+          if(element.price >=100 && element.price<200){
+            return true;
+          }else{
+            return false;
+          }
+          
+        })
+        createDOM(myfiltered);
+        
+      }
+
+      if(myfilterKS712.value==""){
+        createDOM(Forfilter);
+      }
+    })
+    
+    myfilterKS711.addEventListener("change",function(){
+      let myfiltered=Forfilter.filter(function(element,index){
+        if(element.material==myfilterKS711.value){
+          return true;
+        }else{
+          return false;
+        }
+      })
+      createDOM(myfiltered);
+      if(myfilterKS711.value==""){
+        createDOM(Forfilter);
+      }
+    })
+
+
+
+
+
+    let request=fetch("https://embarrassed-bull-moccasins.cyclic.app/products");
+    request.then(function(res){
+      let reqdata=res.json();
+      console.log(reqdata)
+      return reqdata;
+
+    })
+    .then(function(res){
+      
+      console.log(res);
+      Forfilter=res;
+      createDOM(res);
+
+    })
+    .catch(function(rej){
+      console.log(rej);
+    })
+
+ 
+
+
+    function createDOM(data){
+        mycontainer.innerHTML=null;
+
+      data.forEach(function(element,index){
+        let div=document.createElement("div");
+
+        let myimg=document.createElement("img");
+        myimg.setAttribute("src",element.image);
+
+        let mytype=document.createElement("h3");
+        mytype.innerText=element.type;
+
+        let mydesc=document.createElement("p");
+        mydesc.innerText=element.description;
+
+        let mymaterial=document.createElement("p");
+        mymaterial.innerText=element.material;
+
+
+        let priceandcartdiv=document.createElement("div");
+
+
+
+
+
+        let myprice=document.createElement("h3");
+        myprice.innerText=`$ ${element.price}`;
+
+       let myanchortag=document.createElement("a");
+       myanchortag.setAttribute("href","./nextpageprovided by neha")
+
+        
+        let myaddtocartbtn=document.createElement("button");
+        myaddtocartbtn.innerText="Add to Cart";
+        myaddtocartbtn.addEventListener("click",function(){
+
+            LSdata.push(element);
+            
+            
+
+
+
+          
+
+          localStorage.setItem("current",JSON.stringify(LSdata));
+
+        })
+        myanchortag.append(myaddtocartbtn)
+        priceandcartdiv.append(myprice,myanchortag)
+
+
+
+
+
+
+        div.append(myimg,mytype,mydesc,mymaterial,priceandcartdiv);
+
+
+        mycontainer.append(div);
+
+
+      })
+      count=data.length;
+      myheading.innerHTML=count;
+
+
+
+      
+
+
+    }
